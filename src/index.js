4ec480aba5aaf96c8ccc33df124ac81afb9b08e1
Christo-IV <65234973+Christo-IV@users.noreply.github.com>
2021-01-03 15:01:51 +0200

    Update index.js
    Tasks can now be added to the list and they input fields are reset when the "Add Task" button is pressed.

--- src/index.js
+++ src/index.js
@@ -1,61 +1,64 @@
 import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
- 
-// I have no idea how to do this (using React).
+import "./App.css"
 
 const Task = (props) => {
   return (
     <div className="task border">
-      <label>
-        <input type="checkbox" id={props.taskName}></input>
-        {props.taskName}
-      </label>
+      <input type="checkbox" id={props.taskName}></input>
+      <label htmlFor={props.taskName}>{props.taskName}</label>
       <p>{props.taskDate}</p>
     </div>
   )
 }
 
 const Button = (props) => {
-  let textField = document.querySelector('input[type="text"]');
-  let dateField = document.querySelector('input[type="date"]');
-
-  console.log(textField);
-  console.log((textField || {}).value);
-
-  console.log((dateField || {}).value);
-
   return (
-    <button type="button" className="border" onClick={props.setTask("sdsd")}>Add task</button>
+    <button type="button" className="border" onClick={props.addTask}>Add task</button>
   )
 }
 
 const App = () => {
    const [tasks, setTasks] = useState([
-     { name: "First task", date: new Date().toString()},
+     { name: "First task", date: new Date().toDateString()},
    ]);
   const [newTask, setNewTask] = useState("None");
   const [newDate, setNewDate] = useState("None");
 
-  const fSetTasks = () => {
-    setTasks({name: newTask, date: newDate});
-  }
-  
-  const fSetNewTask = (newTask) => {
-    setNewTask(newTask);
+  const checked = document.querySelectorAll('input[type="checkbox"]:checked');
+
+  // useEffect(() => {
+  //   for(let i = 0; i <= checked.length; i++) {
+  //     console.log(checked[i]);
+  //     checked[i].
+  //   };
+  // }, [checked]);
+
+  const fAddTask = () => {
+    let newTaskObject = {};
+    newTaskObject.name = newTask;
+    newTaskObject.date = new Date(newDate).toDateString();
+    setTasks(tasks.concat([newTaskObject]));
+
+    fResetValues();
   }
 
-  const fSetNewDate = (date) => {
-    setNewDate(date);
+  const fResetValues = () => {
+    const textField = document.querySelector('input[type="text"]');
+    const dateField = document.querySelector('input[type="date"]');
+
+    textField.value="";
+    dateField.value="";
   }
 
   return (
   <div className="container">
     <h1>To-do app</h1>
     <form>
-      <input className="border" type="text"></input>
-      <input className="border" type="date"></input>
-      <Button setTask = {fSetNewTask} setDate = {fSetNewDate} setTasks = {fSetTasks}/>
+      <input className="border" type="text" onChange={(event) => {setNewTask(event.target.value)}}></input>
+      <input className="border" type="date" onChange={(event) => {setNewDate(event.target.value)}}></input>
+      <Button addTask = {fAddTask}/>
     </form>
     {tasks.map(task => 
       (<Task key={task.name} taskName={task.name} taskDate={task.date}/>
@@ -63,13 +66,5 @@ const App = () => {
   </div>
   )
 }
-
-/* 
-function convertTime(pDate) {
-  console.log(pDate);
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  return `${pDate.getDate()} ${months[pDate.getMonth()]}`
-}
-*/
 
 ReactDOM.render(<App />, document.getElementById("root"));
